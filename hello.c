19506791b2007d4a74350d432be8d730cc7b65cc
duheng <duheng@streamax.com>
2017-08-09 13:30:21 +0800

    删掉了错误log信息

--- hello.c
+++ hello.c
@@ -4,10 +4,9 @@ int main()
 {
 	printf ("hello world!");
 	printf ("hello git!");
-<<<<<<< HEAD
-//	//for(int i=0;i<5;i++);
+
+	//for(int i=0;i<5;i++);
 =======
 	for(int i=0;i<5;i++);
->>>>>>> 46bc7a06faffebec14cfcc5c1991f2ecb46512cf
 	return 0;
 }
